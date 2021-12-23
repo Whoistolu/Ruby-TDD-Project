38360731dfbde063298808cc868ce5edfd78f951
Whoistolu <digittolu25@gmail.com>
2021-12-23 22:41:23 +0100

    Do TDD for fizzbuzz in solver class

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -29,4 +29,25 @@ describe Solver do
     end
   end
 
+  context 'Test the fizzbuzz method in Solver Class' do
+    it 'should return fizzbuzz if 15 is passed as number' do
+      string = @solver.fizzbuzz(15)
+      expect(string).to eql 'fizzbuzz'
+    end
   
+    it 'should return fizz if 9 is passed as number' do
+      string = @solver.fizzbuzz(9)
+      expect(string).to eql 'fizz'
+    end
+  
+    it 'should return buzz if 10 is passed as number' do
+      string = @solver.fizzbuzz(10)
+      expect(string).to eql 'buzz'
+    end
+  
+    it 'should return 7 as a string it is passed as number' do
+      string = @solver.fizzbuzz(7)
+      expect(string).to eql '7'
+    end
+  end
+  end
