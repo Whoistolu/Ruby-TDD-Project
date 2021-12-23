54e7756d519cb640e76ab31cc44f4eda51bc4543
Whoistolu <digittolu25@gmail.com>
2021-12-23 22:45:24 +0100

    Create the fizzbuzz method in the solver class

--- solver.rb
+++ solver.rb
@@ -17,4 +17,14 @@ class Solver
     string.reverse
   end
 
-  
+  def fizzbuzz(number)
+    if number % 3 == 0 && number % 5 == 0
+      return 'fizzbuzz'
+    elsif number % 3 == 0
+        return 'fizz'
+    elsif number % 5 == 0
+          return 'buzz'
+    else number.to_s
+    end
+  end
+end
